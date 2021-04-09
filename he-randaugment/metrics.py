676a073a9c27aee7f4a7f285593bea88fc85b34a
KF <37930396+KhrystynaFaryna@users.noreply.github.com>
2021-04-09 18:46:11 +0200

    cleaned up unnecessary stuff

--- he-randaugment/metrics.py
+++ he-randaugment/metrics.py
@@ -120,84 +120,6 @@ def accuracy_weighted_fn(y_true, y_pred, label_map=None, threshold=0.5):
 
 # ----------------------------------------------------------------------------------------------------
 
-def auc_weighted_fn(y_true, y_pred, label_map=None):
-
-    # Label map
-    if label_map is not None:
-        # y_true = map_probabilities(y_true, label_map)
-        y_pred = map_probabilities(y_pred, label_map)
-
-    # Weights
-    # w = compute_label_weights(y_true, one_hot=True)
-
-    # Score
-    try:
-        # auc = roc_auc_score(y_true, y_pred, sample_weight=w, average='weighted')
-        # auc = roc_auc_score(y_true, y_pred, average='weighted')
-        auc = roc_auc_score(y_true, y_pred)
-        # print('AUC diff: {a}%'.format(a=np.abs((auc-auc3)/auc*100)))
-
-    except Exception as e:
-        print('AUC could not be computed, returning 0. Exception: %s' % str(e), flush=True)
-        auc = 0
-
-    return auc
-
-# ----------------------------------------------------------------------------------------------------
-
-def roc_binary_weighted(y_true, y_pred, label_map=None, roc_samples=5000):
-
-    def compute_roc(y_tr, y_pr, weight, samples):
-
-        fpr, tpr, th = roc_curve(y_tr, y_pr, pos_label=1, sample_weight=weight)
-        # print(np.max(th))
-
-        # Resample to have same dimensions across trials (10K)
-        th[0] = 1  # avoid interpolation error
-        th[-1] = 0
-        f_fpr = interpolate.interp1d(th, fpr)
-        f_tpr = interpolate.interp1d(th, tpr)
-        new_th = np.linspace(0, 1, samples)  # 1-np.logspace(-3, 0, roc_samples)[:-1]  # avoid zero
-        fpr = f_fpr(new_th)
-        tpr = f_tpr(new_th)
-
-        return fpr, tpr
-
-    # Label map
-    if label_map is not None:
-        # y_true = map_probabilities(y_true, label_map)
-        y_pred = map_probabilities(y_pred, label_map)
-
-    # Weights
-    w = compute_label_weights(y_true, one_hot=True)
-
-    # Score
-    try:
-        if y_true.shape[1] == 2:
-            fpr, tpr = compute_roc(y_true[:, 1], y_pred[:, 1], weight=w, samples=roc_samples)
-        else:
-            # Multiclass average ROCs
-            print('Computing multiclass ROC (macro average)')
-            # TODO balance class average with weights
-            fprs = []
-            tprs = []
-            for i in range(y_true.shape[1]):
-                fpr, tpr = compute_roc(y_true[:, i], y_pred[:, i], weight=w, samples=roc_samples)
-                fprs.append(fpr)
-                tprs.append(tpr)
-
-            fpr = np.mean(np.stack(fprs, axis=1), axis=1)
-            tpr = np.mean(np.stack(tprs, axis=1), axis=1)
-
-    except Exception as e:
-        print('ROC could not be computed, returning []. Exception: %s' % str(e), flush=True)
-        fpr = []
-        tpr = []
-
-    return fpr, tpr
-
-# ----------------------------------------------------------------------------------------------------
-
 def log_loss_fn(y_true, y_pred):
 
     return log_loss(y_true, y_pred)
@@ -210,13 +132,5 @@ def log_loss_weighted_fn(y_true, y_pred):
 
     return log_loss(y_true, y_pred, sample_weight=w)
 
-# ----------------------------------------------------------------------------------------------------
-
-def spearman_fn(y_true, y_pred):
-    return spearmanr(y_true, y_pred)[0]
-# ----------------------------------------------------------------------------------------------------
-
-def mse_fn(y_true, y_pred):
-    return np.mean(np.square(y_true - y_pred))
 
 
