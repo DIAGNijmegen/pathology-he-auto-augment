d901d2f54615d12fd0ebcb10682ac80316602f5f
KF <37930396+KhrystynaFaryna@users.noreply.github.com>
2021-04-09 18:32:50 +0200

    Cleaned up unnecessary files

--- he-randaugment/data_augmentation.py
+++ he-randaugment/data_augmentation.py
@@ -3,17 +3,9 @@
 
 import matplotlib as mpl
 mpl.use('Agg')  # plot figures when no screen available
-
-from augmenters.noise.gaussianbluraugmenter import GaussianBlurAugmenter
-from augmenters.color.contrastaugmenter import ContrastAugmenter
-from augmenters.color.hedcoloraugmenter import HedColorAugmenter
-from augmenters.color.hsbcoloraugmenter import HsbColorAugmenter
-from augmenters.noise.additiveguassiannoiseaugmenter import AdditiveGaussianNoiseAugmenter
 from augmenters.passthroughaugmenter import PassThroughAugmenter
-from augmenters.spatial.elasticagumenter import ElasticAugmenter
 from augmenters.spatial.flipaugmenter import FlipAugmenter
 from augmenters.spatial.rotate90augmenter import Rotate90Augmenter
-from augmenters.spatial.scalingaugmenter import ScalingAugmenter
 from augmenters.color import coloraugmenterbase
 
 import numpy as np
@@ -63,7 +55,7 @@ def rgb_to_gray(batch):
     return new_batch
 
 def define_augmenters(augmentation_tag):
-
+    #Note that this is not the baseline from paper
     if augmentation_tag == 'baseline':
 
          augmenters = [
@@ -72,23 +64,6 @@ def define_augmenters(augmentation_tag):
          ]
 
 
-
-    elif augmentation_tag == 'hed-light':
-
-         augmenters = [
-            ('rotate90', Rotate90Augmenter(k_list=[0, 1, 2, 3])),
-            ('flip', FlipAugmenter(flip_list=['none', 'vertical', 'horizontal', 'both'])),
-            ('scaling', ScalingAugmenter(scaling_range=(0.8, 1.2), interpolation_order=1)),
-            ('elastic', ElasticAugmenter(sigma_interval=(9.0, 11.0), alpha_interval=(80.0, 120.0), map_count=50, interpolation_order=1)),
-            ('hed_stain', HedColorAugmenter(haematoxylin_sigma_range=(-0.05, 0.05), haematoxylin_bias_range=(-0.05, 0.05),
-                                            eosin_sigma_range=(-0.05, 0.05), eosin_bias_range=(-0.05, 0.05),
-                                            dab_sigma_range=(-0.05, 0.05), dab_bias_range=(-0.05, 0.05),
-                                            cutoff_range=(0.15, 0.85))),
-            ('brightness_enh', BrightnessEnhAugmenter(sigma_range=[0.65, 1.35])),
-            ('contrast_enh', ContrastEnhAugmenter(sigma_range=[0.5, 1.5])),
-            ('additive', AdditiveGaussianNoiseAugmenter(sigma_range=(0.0, 0.1))),
-            ('blur', GaussianBlurAugmenter(sigma_range=(0.1, 1)))
-         ]
 
     elif augmentation_tag == 'none':
 
@@ -104,158 +79,3 @@ def define_augmenters(augmentation_tag):
 
 
 #----------------------------------------------------------------------------------------------------
-
-class BrightnessEnhAugmenter(coloraugmenterbase.ColorAugmenterBase):
-
-    def __init__(self, sigma_range):
-
-        # Initialize base class.
-        #
-        super().__init__(keyword='brightness')
-
-        # Initialize members.
-        #
-        self.__sigma_range = None
-        self.__sigma = None
-
-        # Save configuration.
-        #
-        self.__setsigmaranges(sigma_range=sigma_range)
-
-    def __setsigmaranges(self, sigma_range):
-
-        # Store the setting.
-        #
-        self.__sigma_range = sigma_range
-
-    def transform(self, patch):
-
-        # Prepare
-        rgb_space = True if patch.shape[0] == 3 else False
-        patch_image = np.transpose(a=patch, axes=(1, 2, 0))
-        image = Image.fromarray(np.uint8(patch_image if rgb_space else patch_image[:, :, 0]))
-
-        # Change brightness
-        enhanced_image = ImageEnhance.Brightness(image)
-        enhanced_image = enhanced_image.enhance(self.__sigma)
-
-        # Convert back
-        patch_enhanced = np.asarray(enhanced_image)
-        patch_enhanced = patch_enhanced if rgb_space else patch_enhanced[:, :, np.newaxis]
-        patch_enhanced = patch_enhanced.astype(dtype=np.uint8)
-        patch_enhanced = np.transpose(a=patch_enhanced, axes=(2, 0, 1))
-
-        return patch_enhanced
-
-    def randomize(self):
-        """Randomize the parameters of the augmenter."""
-
-        # Randomize sigma for each channel.
-        #
-        self.__sigma = np.random.uniform(low=self.__sigma_range[0], high=self.__sigma_range[1], size=None)
-
-
-#----------------------------------------------------------------------------------------------------
-
-class ContrastEnhAugmenter(coloraugmenterbase.ColorAugmenterBase):
-
-    def __init__(self, sigma_range):
-
-        # Initialize base class.
-        #
-        super().__init__(keyword='contrast_enh')
-
-        # Initialize members.
-        #
-        self.__sigma_range = None
-        self.__sigma = None
-
-        # Save configuration.
-        #
-        self.__setsigmaranges(sigma_range=sigma_range)
-
-    def __setsigmaranges(self, sigma_range):
-
-        # Store the setting.
-        #
-        self.__sigma_range = sigma_range
-
-    def transform(self, patch):
-
-        # Prepare
-        rgb_space = True if patch.shape[0] == 3 else False
-        patch_image = np.transpose(a=patch, axes=(1, 2, 0))
-        image = Image.fromarray(np.uint8(patch_image if rgb_space else patch_image[:, :, 0]))
-
-        # Change brightness
-        enhanced_image = ImageEnhance.Contrast(image)
-        enhanced_image = enhanced_image.enhance(self.__sigma)
-
-        # Convert back
-        patch_enhanced = np.asarray(enhanced_image)
-        patch_enhanced = patch_enhanced if rgb_space else patch_enhanced[:, :, np.newaxis]
-        patch_enhanced = patch_enhanced.astype(dtype=np.uint8)
-        patch_enhanced = np.transpose(a=patch_enhanced, axes=(2, 0, 1))
-
-        return patch_enhanced
-
-    def randomize(self):
-        """Randomize the parameters of the augmenter."""
-
-        # Randomize sigma for each channel.
-        #
-        self.__sigma = np.random.uniform(low=self.__sigma_range[0], high=self.__sigma_range[1], size=None)
-
-
-#----------------------------------------------------------------------------------------------------
-
-class ColorEnhAugmenter(coloraugmenterbase.ColorAugmenterBase):
-
-    def __init__(self, sigma_range):
-
-        # Initialize base class.
-        #
-        super().__init__(keyword='color_enh')
-
-        # Initialize members.
-        #
-        self.__sigma_range = None
-        self.__sigma = None
-
-        # Save configuration.
-        #
-        self.__setsigmaranges(sigma_range=sigma_range)
-
-    def __setsigmaranges(self, sigma_range):
-
-        # Store the setting.
-        #
-        self.__sigma_range = sigma_range
-
-    def transform(self, patch):
-
-        # Prepare
-        rgb_space = True if patch.shape[0] == 3 else False
-        patch_image = np.transpose(a=patch, axes=(1, 2, 0))
-        image = Image.fromarray(np.uint8(patch_image if rgb_space else patch_image[:, :, 0]))
-
-        # Change brightness
-        enhanced_image = ImageEnhance.Color(image)
-        enhanced_image = enhanced_image.enhance(self.__sigma)
-
-        # Convert back
-        patch_enhanced = np.asarray(enhanced_image)
-        patch_enhanced = patch_enhanced if rgb_space else patch_enhanced[:, :, np.newaxis]
-        patch_enhanced = patch_enhanced.astype(dtype=np.uint8)
-        patch_enhanced = np.transpose(a=patch_enhanced, axes=(2, 0, 1))
-
-        return patch_enhanced
-
-    def randomize(self):
-        """Randomize the parameters of the augmenter."""
-
-        # Randomize sigma for each channel.
-        #
-        self.__sigma = np.random.uniform(low=self.__sigma_range[0], high=self.__sigma_range[1], size=None)
-
-
