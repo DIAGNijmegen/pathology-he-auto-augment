0cdb262cabc8e7bdced8dfcd452a7c9085125df7
KF <37930396+KhrystynaFaryna@users.noreply.github.com>
2021-04-28 17:13:10 +0200

    Update custom_hed_transform.py
    cleaned up

--- he-randaugment/custom_hed_transform.py
+++ he-randaugment/custom_hed_transform.py
@@ -21,28 +21,6 @@ def hed2rgb(hed):
 
 def separate_stains(rgb, conv_matrix):
 
-    # # t = time.time()
-    # rgb = dtype.img_as_float(rgb, force_copy=True).astype('float32')
-    # # print('{f} took {s} s'.format(f='separate img_as_float', s=(time.time() - t)), flush=True)
-    # # print('rgb type is {r}, matrix type is {m}'.format(r=rgb.dtype, m=conv_matrix.dtype), flush=True)
-    #
-    # # t = time.time()
-    # rgb = -np.log(rgb)
-    # # print('{f} took {s} s'.format(f='separate np.log', s=(time.time() - t)), flush=True)
-    #
-    # # t = time.time()
-    # rgb += 2
-    # rgb = np.reshape(rgb, (-1, 3))
-    # # print('{f} took {s} s'.format(f='separate add reshape', s=(time.time() - t)), flush=True)
-    #
-    # # print('x shape is {s}, conv_matrix shape is {c}'.format(s=x.shape, c=conv_matrix.shape))
-    #
-    # # t = time.time()
-    # stains = np.dot(rgb, conv_matrix)
-    # # print('{f} took {s} s'.format(f='separate np.dot', s=(time.time() - t)), flush=True)
-    #
-    # return np.reshape(stains, rgb.shape)
-
     rgb = dtype.img_as_float(rgb, force_copy=True).astype('float32')
     rgb += 2
     stains = np.dot(np.reshape(-np.log(rgb), (-1, 3)), conv_matrix)
@@ -51,30 +29,6 @@ def separate_stains(rgb, conv_matrix):
 
 def combine_stains(stains, conv_matrix):
 
-    # # t = time.time()
-    # stains = dtype.img_as_float(stains).astype('float32')
-    # # stains = stains.astype('float32')
-    # # conv_matrix = conv_matrix.astype('float32')
-    # # print('{f} took {s} s'.format(f='separate img_as_float', s=(time.time() - t)), flush=True)
-    # # print('stains type is {r}, matrix type is {m}'.format(r=stains.dtype, m=conv_matrix.dtype), flush=True)
-    #
-    # stains = -np.reshape(stains, (-1, 3))
-    # # print('x shape is {s}, conv_matrix shape is {c}'.format(s=x.shape, c=conv_matrix.shape))
-    #
-    # # t = time.time()
-    # logrgb2 = np.dot(stains, conv_matrix)
-    # # print('{f} took {s} s'.format(f='combine np.dot', s=(time.time() - t)), flush=True)
-    #
-    # # t = time.time()
-    # rgb2 = np.exp(logrgb2)
-    # # print('{f} took {s} s'.format(f='combine np.exp', s=(time.time() - t)), flush=True)
-    #
-    # t = time.time()
-    # x = rescale_intensity(np.reshape(rgb2 - 2, stains.shape),
-    #                          in_range=(-1, 1))
-    # print('{f} took {s} s'.format(f='combine rescale_intensity', s=(time.time() - t)), flush=True)
-    #
-    # return x
 
     stains = dtype.img_as_float(stains.astype('float64')).astype('float32')  # stains are out of range [-1, 1] so dtype.img_as_float complains if not float64
     logrgb2 = np.dot(-np.reshape(stains, (-1, 3)), conv_matrix)
